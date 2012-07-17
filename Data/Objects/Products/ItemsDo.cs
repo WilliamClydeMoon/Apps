514199e9d0c5b960f14daa48b81ccf203b3e77ff
Bill Moon <williamclydemoon@yahoo.com>
2012-07-17 13:34:45 -0500

    Added logic to ItemsDo.cs

--- Data/Objects/Products/ItemsDo.cs
+++ Data/Objects/Products/ItemsDo.cs
@@ -7,5 +7,24 @@ namespace Data.Objects.Products
 {
     public class ItemsDo
     {
+        private List<Item> _Items;
+        public List<string> Ids  = new List<string>();
+
+        private string _Id;
+
+        private bool _getall;
+        private int _idtype;
+
+
+        #region Contstructor
+        /*__________________________________________________________________________________________*/
+        public ItemsDo()
+        {
+          
+            Items = new List<Item>();
+        }
+        #endregion
+        /*__________________________________________________________________________________________*/
+        public List<Item> Items { get; set; }
     }
 }
